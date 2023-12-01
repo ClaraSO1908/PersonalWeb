16cfab3705626331f0354aa59f45429cba1eac7e
Clara Sacedón Ortega <clara.so1908@gmail.com>
2023-12-01 14:58:26 +0100

    Added Schema.org markup and code validations

---
+++ assets/js/json-ld.js
@@ -0,0 +1,20 @@
+// JSON-LD
+var jsonLdData = {
+    "@context": "http://www.schema.org",
+    "@type": "person",
+    "name": "Clara Sacedón Ortega",
+    "jobTitle": "Ingeniería Informática",
+    "url": "https://clarasacedon.netlify.app/",
+    "address": {
+      "@type": "PostalAddress",
+      "addressLocality": "Madrid",
+      "addressRegion": "Madrid",
+      "addressCountry": "España"
+    },
+    "email": "clara.sacedon@gmail.com",
+    "sameAs": [
+      "https://www.linkedin.com/in/clara-sacedon-ortega/",
+      "https://github.com/clarasacedon",
+      "https://t.me/clarasortega"
+    ]
+};
